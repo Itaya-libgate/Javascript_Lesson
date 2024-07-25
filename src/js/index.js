d736ef29b8e3d86c139f3668276ff48bfff330dd
itaya_daiya <itaya.daiya@lib-gate.co.jp>
2024-07-26 00:23:26 +0900

    シングルクォーテーションへ変更

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 // JavaScriptファイルをブラウザ上のコンソールで使用できるかの確認
-let main = "hello"; // 常に1:1
+let main = 'hello'; // 常に1:1
 console.log(main);
 
 // 定数
-const Second = "hello2" // 常に1:1
+const Second = 'hello2' // 常に1:1
 console.log(Second);
 
 // 配列
-let array = ["hello", "hello2", "hello3"];
+let array = ['hello', 'hello2', 'hello3'];
 console.log(array);
 console.log(array[2]); // 配列の特定の場所を出力
 console.log(array[0]); // 配列の特定の場所を出力
@@ -30,9 +30,9 @@ while(index2 < array.length){
 
 // if else文
 if(array.length > 2){
-    console.log("でたー！")
+    console.log('でたー！')
   } else {
-    console.log("でないー！")
+    console.log('でないー！')
   }
 
 
@@ -46,9 +46,9 @@ test();
 // 関数の例(引数なし)
 const test2 = () => {
   if(array.length > 2){
-    console.log("でたー！")
+    console.log('でたー！')
   } else {
-    console.log("でないー！")
+    console.log('でないー！')
   }
 }
 test2();
@@ -58,9 +58,9 @@ test2();
 // 引数を使用することで、流用が効くようになる。
 const test3 = (arg) => {
   if(array.length > arg){
-    console.log("でたー！")
+    console.log('でたー！')
   } else {
-    console.log("でないー！")
+    console.log('でないー！')
   }
 }
 test3(5);
@@ -69,12 +69,12 @@ test3(5);
 // 変数や定数は1:1なのに対して、オブジェクトは複数の値を持つことができる　＝　データの塊
 // オブジェクト内に関数も入れることができる
 const test4 = {
-  name: "hello",
+  name: 'hello',
   age: 28,
-  from: "Japan",
+  from: 'Japan',
   // 関数を挿入する
   test5: () => {
-    console.log("test5");
+    console.log('test5');
   }
 }
 console.log(test4);
@@ -90,5 +90,5 @@ console.log(document.getElementsByTagName('button'))
 // 何かボタンをクリックしたときに、画面をスクロールしたときなど、ユーザーが何かをしたときに何らかの挙動を追加する際に使うオブジェクト
 document.getElementsByTagName('button')[0].addEventListener('click', ()=> {
   // 何らかの挙動が起きたときの処理を記述する
-  window.alert("Hello World");
+  window.alert('Hello World');
 });//addEventListenerには引数が２つ設定しないといけない　1つ目：eventのタイプを入れる(例)：クリックする（click）)　2つ目：
